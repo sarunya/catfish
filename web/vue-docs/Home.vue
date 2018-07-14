c415ddca51777cdb4e53ea418636bc9c7f9e703e
Sarunya Durai <sarunya.d@samsung.com>
2018-07-15 02:44:32 +0530

    html to vue

---
+++ web/vue-docs/Home.vue
@@ -0,0 +1,276 @@
+<!--
+	Author: W3layouts
+	Author URL: http://w3layouts.com
+	License: Creative Commons Attribution 3.0 Unported
+	License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<template>
+
+
+<div>
+	<div class="top_header" id="home">
+		<!-- Fixed navbar -->
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="nav_top_fx_w3ls_agileinfo">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false"
+					    aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+					<div class="logo-w3layouts-agileits">
+						<h1> <a class="navbar-brand" href="index.html"><i class="fa fa-clone" aria-hidden="true"></i> CatFish <span class="desc">For your Development</span></a></h1>
+					</div>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<div class="nav_right_top">
+						
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="index.html">Home</a></li>
+						</ul>
+					</div>
+				</div>
+				<!--/.nav-collapse -->
+			</div>
+		</nav>
+	</div>
+	<!-- banner -->
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<!-- <ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1" class=""></li>
+			<li data-target="#myCarousel" data-slide-to="2" class=""></li>
+			<li data-target="#myCarousel" data-slide-to="3" class=""></li>
+		</ol> -->
+		<div class="carousel-inner" role="listbox">
+			<div class="item active">
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>Developer Assistant</h3>
+						<p>manage your things here</p>
+						<div class="top-buttons">
+							<div class="bnr-button">
+								<a class="act" href="single.html">Interview Zone</a>
+							</div>
+							<div class="bnr-button">
+								<a href="portfolio.html" class="two scroll ">Tunnels</a>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- <div class="item item2">
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>Best Business Thinking</h3>
+						<p>You deserve the best</p>
+						<div class="top-buttons">
+							<div class="bnr-button">
+								<a class="act" href="single.html">Code Share</a>
+							</div>
+							<div class="bnr-button">
+								<a href="portfolio.html" class="two scroll ">Tunnels</a>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="item item3">
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>Build Effective Designs</h3>
+						<p>Solutions made easy</p>
+						<div class="top-buttons">
+							<div class="bnr-button">
+								<a class="act" href="single.html">Code Share</a>
+							</div>
+							<div class="bnr-button">
+								<a href="portfolio.html" class="two scroll ">Tunnels</a>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div class="item item4">
+				<div class="container">
+					<div class="carousel-caption">
+
+						<h3>Best Business Thinking</h3>
+						<p>You deserve the best</p>
+						<div class="top-buttons">
+							<div class="bnr-button">
+								<a class="act" href="single.html">Code Share</a>
+							</div>
+							<div class="bnr-button">
+								<a href="#portfolio" class="two scroll ">Tunnels</a>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+				</div>
+			</div> -->
+		</div>
+		<!-- <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="fa fa-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="fa fa-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a> -->
+		<!-- The Modal -->
+	</div>
+	<!--//banner -->
+	<!--/ab-->
+	<div class="banner_bottom">
+		<div class="container">
+			<h3 class="tittle-w3ls">About Us</h3>
+			<div class="inner_sec_info_wthree_agile">
+				<div class="help_full">
+
+					<div class="col-md-6 banner_bottom_grid help">
+						<img src="images/ab.png" alt=" " class="img-responsive">
+					</div>
+					<div class="col-md-6 banner_bottom_left">
+						<h4>Working to build a better web design</h4>
+						<p>Pellentesque convallis diam consequat magna vulputate malesuada. Cras a ornare elit. Nulla viverra pharetra sem, eget
+							pulvinar neque pharetra ac.</p>
+						<p>Lorem Ipsum convallis diam consequat magna vulputate malesuada. Cras a ornare elit. Nulla viverra pharetra sem, eget
+							pulvinar neque pharetra ac.</p>
+						<div class="ab_button">
+							<a class="btn btn-primary btn-lg hvr-underline-from-left" href="single.html" role="button">Read More </a>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+	<!--//ab-->
+	<!--/what-->
+	
+	<!--//what-->
+	<!--/banner_bottom-->
+	<!--//banner_bottom-->
+	<!--/projects-->
+	
+	<!--//projects-->
+	<!--/blog-->
+	
+	<!--//blog-->
+	<!--/bottom-->
+	<!-- footer -->
+	<div class="footer">
+		<div class="footer_inner_info_w3ls_agileits">
+			<div class="col-md-3 footer-left">
+				<h2><a href="index.html"><i class="fa fa-clone" aria-hidden="true"></i> Conceit </a></h2>
+				<p>Lorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora.</p>
+				<ul class="social-nav model-3d-0 footer-social social two">
+					<li>
+						<a href="#" class="facebook">
+							<div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+							<div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+						</a>
+					</li>
+					<li>
+						<a href="#" class="twitter">
+							<div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+							<div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+						</a>
+					</li>
+					<li>
+						<a href="#" class="instagram">
+							<div class="front"><i class="fa fa-instagram" aria-hidden="true"></i></div>
+							<div class="back"><i class="fa fa-instagram" aria-hidden="true"></i></div>
+						</a>
+					</li>
+					<li>
+						<a href="#" class="pinterest">
+							<div class="front"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
+							<div class="back"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-md-9 footer-right">
+				<div class="sign-grds">
+					<div class="col-md-4 sign-gd">
+						<h4>Latest <span>Info</span> </h4>
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="about.html">About</a></li>
+							<li><a href="404.html">Services</a></li>
+							<li><a href="signin.html">Signin</a></li>
+							<li><a href="contact.html">Contact</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3 sign-gd flickr-post">
+						<h4>Our <span>Works</span></h4>
+						<ul>
+							<li><a href="single.html"><img src="images/g1.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g2.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g3.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g4.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g5.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g6.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g7.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g8.jpg" alt=" " class="img-responsive" /></a></li>
+							<li><a href="single.html"><img src="images/g4.jpg" alt=" " class="img-responsive" /></a></li>
+						</ul>
+					</div>
+					<div class="col-md-5 sign-gd-two">
+						<h4>Contact <span>Information</span></h4>
+						<div class="address">
+							<div class="address-grid">
+								<div class="address-left">
+									<i class="fa fa-phone" aria-hidden="true"></i>
+								</div>
+								<div class="address-right">
+									<h6>Phone Number</h6>
+									<p>+1 234 567 8901</p>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+							<div class="address-grid">
+								<div class="address-left">
+									<i class="fa fa-envelope" aria-hidden="true"></i>
+								</div>
+								<div class="address-right">
+									<h6>Email Address</h6>
+									<p>Email :<a href="mailto:example@email.com"> mail@example.com</a></p>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+							<div class="address-grid">
+								<div class="address-left">
+									<i class="fa fa-map-marker" aria-hidden="true"></i>
+								</div>
+								<div class="address-right">
+									<h6>Location</h6>
+									<p>Broome St, NY 10002,California, USA.
+
+									</p>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+						</div>
+					</div>
+
+					<div class="clearfix"></div>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+			<p class="copy-right">copy 2018 Conceit. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
+		</div>
+	</div>
+</div>
+    
+</template>
