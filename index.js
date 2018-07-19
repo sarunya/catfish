914be4199418fa8ccabaa9431176854668cf59b1
eCOM-Bala.ms <bala.ms@samsung.com>
2018-07-19 23:47:03 +0530

    check

--- index.js
+++ index.js
@@ -1,48 +1,9 @@
 var express = require('express');
+var app = express();
 var path = require('path');
 
-var app = express();
 
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-
+app.use(express.static(path.join(__dirname , 'web')));
 
 var port = process.env.PORT || 1337;
 app.listen(port);
