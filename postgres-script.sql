91d5c9a9866759939a1657caa80f99587444c559
Sarunya Durai <sarunya.d@samsung.com>
2018-12-28 18:30:36 +0530

    push some changes

--- postgres-script.sql
+++ postgres-script.sql
@@ -19,3 +19,15 @@ CREATE TABLE IF NOT EXISTS codeshare_data
 WITH (
   OIDS=FALSE
 );
+
+CREATE TABLE IF NOT EXISTS task_data
+(
+  id uuid,
+  identity_id VARCHAR,
+  data jsonb,
+  created_date timestamp with time zone DEFAULT now(),
+  modified_date timestamp with time zone DEFAULT now()
+)
+WITH (
+  OIDS=FALSE
+);
